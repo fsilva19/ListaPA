682a50456e77de34436fd330c3db749d258b4991
Felipe Gabriel <fsilva19@ufrn.edu.br>
2023-05-02 22:59:28 -0300

    readme

--- questao11.c
+++ questao11.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(void){
     float aloha[10], coisas[10][5], *pf, value = 2.2;
-    aloha[2] = value; //=> VÁLIDO, o elemento de posição 2 recebe o valor de value
-    scanf("%f", &aloha); //
-    printf("%f", *aloha);
+    aloha[2] = value; // VÁLIDO, o elemento de posição 2 recebe o valor de value
+    scanf("%f", &aloha); // INVÁLIDO, aloha é um vetor, e não pode ser passado como um único valor
+    aloha = value; // INVÁLIDO, mesma coisa de cima, aloha é um vetor de 10 elementos
+    printf("%f", aloha); // INVÁLIDO, aloha é um vetor
+    coisas[4][4] = aloha[3]; // VÁLIDO, o elemento linha4 coluna4 de coisas, recebe o elementos 3 de aloha
+    pf = value; // INVÁLIDO, precisa informar que é o ponteiro para o enedereço (&value)
+    pf = aloha;// VÁLIDO, precisa informar que é o ponteiro para o enedereço (&aloha)
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Repositório das questões Lista Ponteiros - Programação Avançada.
+### Discente:
+#### Felipe Gabriel B. da Silva - 20200069600
